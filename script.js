d3c1bd3f68cdf53b548d92a384fe0fa91d11a538
sachin <sachinlunayach333@gmail.com>
2023-10-04 11:43:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const animatedElements = document.querySelectorAll(".fade-in");
+
+    function checkElements() {
+        animatedElements.forEach((element) => {
+            const elementTop = element.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (elementTop < windowHeight - 100) {
+                element.classList.add("active");
+            }
+        });
+    }
+
+    // Initial check when the page loads
+    checkElements();
+
+    // Add a scroll event listener to trigger the animation as you scroll
+    window.addEventListener("scroll", checkElements);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const animatedElements = document.querySelectorAll(".fade-out");
+
+    function checkElements() {
+        animatedElements.forEach((element) => {
+            const elementTop = element.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (elementTop < windowHeight - 100) {
+                element.classList.add("active");
+            }
+        });
+    }
+    window.addEventListener("scroll", checkElements);
+
+    // Initial check when the page loads
+    checkElements();
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const animatedElements = document.querySelectorAll(".card-out");
+
+    function checkElements() {
+        animatedElements.forEach((element) => {
+            const elementTop = element.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (elementTop < windowHeight - 100) {
+                element.classList.add("active");
+            }
+        });
+    }
+    window.addEventListener("scroll", checkElements);
+
+    // Initial check when the page loads
+    checkElements();
+
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.querySelector(".animate-navbar");
+
+    // Apply the final transformation and fade-in effect
+    navbar.style.transform = "translateY(0)";
+    navbar.style.opacity = "1";
+});
+
+
+
+
